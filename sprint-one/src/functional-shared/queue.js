3ea620831e8d75179aa03a9254abc6d7339b1120
clutejoshua <57841091+clutejoshua@users.noreply.github.com>
2020-03-26 16:55:04 -0700

    Used the keyword this to implement queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,37 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = {
+    first: 1,
+    last: 1
+  };
+
+  _.extend(storage, queueMethods);
+
+  return storage;
 };
 
-var queueMethods = {};
+var queueMethods = {
+
+  enqueue: function(val) {
+    this[this.first] = val;
+    this.first++;
+  },
+
+  dequeue: function() {
+    var popped = this[this.last];
+    delete this[this.last];
+    this.last++;
+    return popped;
+  },
+
+  size: function() {
+    if (this.first <= this.last) {
+      return 0;
+    }
+    return this.first - this.last;
+  }
+
+};
 
 
