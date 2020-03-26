fb79bb10524471e7eab4449d38618e1ac7636cd3
clutejoshua <57841091+clutejoshua@users.noreply.github.com>
2020-03-26 16:44:27 -0700

    Functional shared stack with Rongyi

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,36 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var storage = {
+    key: 1
+  };
+
+  _.extend(storage, sharedMethods);
+
+  return storage;
 };
 
-var stackMethods = {};
+sharedMethods = {
 
+  push: function(val) {
+    this[this.key] = val;
+    this.key++;
+  },
+
+  pop: function() {
+    // debugger;
+    var popped = this[this.key - 1];
+    delete this[this.key - 1];
+    this.key--;
+    return popped;
+  },
+
+  size: function () {
+    if (this.key <= 0) {
+      return 0;
+    }
+    return this.key - 1;
+  }
+};
+
+// someStack = Stack();
+// someStack.push('a');
 
