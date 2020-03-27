830f1f8cef0313d03b26171ae817f017433ca905
clutejoshua <57841091+clutejoshua@users.noreply.github.com>
2020-03-26 17:15:25 -0700

    Prototypal stack with Rongyi

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,39 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var instance = Object.create(stackMethods);
+  instance.key = 1;
+  instance.storage = {};
+  return instance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    this.storage[this.key] = value;
+    this.key++;
+  },
 
+  pop: function() {
+    var popped = this.storage[this.key - 1];
+    delete this.storage[this.key];
+    this.key--;
+    return popped;
+  },
 
+  size: function() {
+    if (this.key <= 0) {
+      return 0;
+    }
+    return this.key - 1;
+  }
+
+};
+
+// someInstance = stack();
+// someInstance.push("a");
+
+// {
+//   storage:{}
+//   key : 1
+//   push
+//   pop
+//   size
+// }
