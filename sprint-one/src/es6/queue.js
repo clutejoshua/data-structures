4c3b1ad787c16298ced04d24abfc9182b45c6dee
clutejoshua <57841091+clutejoshua@users.noreply.github.com>
2020-03-27 15:20:37 -0700

    ES6 instantiation pattern for Queue and Stack

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,31 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.first = 1;
+    this.last = 1;
   }
 
+  enqueue(val) {
+    this.storage[this.first] = val;
+    this.first++;
+  }
+
+  dequeue() {
+    var popped = this.storage[this.last];
+    delete this.storage[this.last];
+    this.last++;
+    return popped;
+  }
+
+  size() {
+    if (this.first <= this.last) {
+      return 0;
+    }
+    return this.first - this.last;
+  }
 }
+
+// var test = new Queue();
+// console.log(test.enqueue("a"));
+// console.log(test);
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,35 @@
 class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  // Work within the src/es6/ folder
+  // Capitalize your function name to indicate to others that it is intended to be run with the keyword new
+  // Use the keyword new when instantiating your class
+  // Use the keyword this in your constructor
+  // Explicitly declare a class method named constructor
+  // Declare all other class methods within the class declaration
+
   constructor() {
+    this.storage = {};
+    this.key = 1;
   }
 
+  push(value) {
+    this.storage[this.key] = value;
+    this.key++;
+  }
+
+  pop() {
+    var popped = this.storage[this.key - 1];
+    delete this.storage[this.key - 1];
+    this.key--;
+    return popped;
+  }
+
+  size() {
+    if (this.key <= 0) {
+      return 0;
+    }
+    return this.key - 1;
+  }
 }
+
