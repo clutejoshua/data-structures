5465bebe2ef0b4e8eeb3d40f05d4671a74aef5b0
clutejoshua <57841091+clutejoshua@users.noreply.github.com>
2020-03-28 12:30:27 -0700

    Hashtable insertion and retrieval methods working with Rongyi

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,16 +5,27 @@ var HashTable = function() {
   this._storage = LimitedArray(this._limit);
 };
 
+// O(1) time | O(1) space
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  let bucket = this._storage.get(index);
+  this._storage.set(index, v);
 };
 
+// O(1) time | O(1) space
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  let bucket = this._storage.get(index);
+  return bucket;
 };
 
+// O(1) time | O(1) space
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  let bucket = this._storage.get(index);
+  if (bucket === k) {
+    delete bucket;
+  }
 };
 
 
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,37 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = 0;
   return set;
 };
 
 var setPrototype = {};
 
+// O(1) time | O(1) space
 setPrototype.add = function(item) {
+  if (this.item === undefined) {
+    this[this._storage] = item;
+    this._storage++;
+  }
+  return this;
 };
 
+// O(n) time | O(1) space
 setPrototype.contains = function(item) {
+  for (let key in this) {
+    if (this[key] === item) {
+      return true;
+    }
+  }
+  return false;
 };
 
+// O(n) time | O(1) space
 setPrototype.remove = function(item) {
+  for (let key in this) {
+    if (this[key] === item) {
+      delete this[key];
+    }
+  }
 };
 
 /*
